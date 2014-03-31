3d94b1f7aa1bb2dc5380ad6b78290e0de514aac9
serin <serin212@hotmail.com>
2014-03-31 11:33:50 +0700

    Add greensock

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -9,10 +9,12 @@ window.App = {
 
 require.config({
     paths: {
-        'jquery'          : 'vendor/jquery/dist/jquery.min',
-        'jquery.mockjax'  : 'vendor/mockjax/jquery.mockjax',
-        'underscore'      : 'vendor/underscore/underscore',
-        'backbone'        : 'vendor/backbone/backbone'
+        'jquery'            : 'vendor/jquery/dist/jquery.min',
+        'jquery.mockjax'    : 'vendor/mockjax/jquery.mockjax',
+        'underscore'        : 'vendor/underscore/underscore',
+        'backbone'          : 'vendor/backbone/backbone',
+        'TweenMax'          : 'vendor/greensock/src/minified/TweenMax.min',
+        'TimelineMax'       : 'vendor/greensock/src/minified/TimelineMax.min'
     },
     shim: {
         'jquery.mockjax':{
